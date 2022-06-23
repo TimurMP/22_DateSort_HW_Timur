51c5b5def3af66fc91b73faa2a9d1b2e494dcf1d
TimurMP <titimurmamyshev@gmail.commur>
2022-06-24 02:07:46 +0300

    Baseline

--- src/telran/datesort/tests/DateSortTest.java
+++ src/telran/datesort/tests/DateSortTest.java
@@ -1,4 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
-class claTest {
-  
+package telran.datesort.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class DateSortTest {
+
+
+    @Test
+    void test() {
+        String[] dates = {
+                "07-05-1990",
+                "28-01-2010",
+                "11-08-1990",
+                "15-01-2010",
+                "16/06/1970"
+        };
+
+        String[] expected = {
+                "16/06/1970",
+                "07-05-1990",
+                "11-08-1990",
+                "15-01-2010",
+                "28-01-2010",
+        };
+        Comparator<String> comparator = (d1,d2) -> 0;
+
+        Arrays.sort(dates, comparator);
+
+        assertArrayEquals(expected, dates);
+
+
+
+    }
 }
