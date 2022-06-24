5bad12985fe912b7acbace2f15951ef88e4f4933
TimurMP <titimurmamyshev@gmail.commur>
2022-06-24 11:42:51 +0300

    Done?

--- src/telran/datesort/tests/DateSortTest.java
+++ src/telran/datesort/tests/DateSortTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 class DateSortTest {
 
 
@@ -25,17 +27,34 @@ class DateSortTest {
                 "15-01-2010",
                 "28-01-2010",
         };
-        Comparator<String> comparator = (d1, d2) -> 0;
+        Comparator<String> comparator = (d1, d2) -> {
+            if (d1.substring(6, 10).compareTo(d2.substring(6,10)) < 0){
+                return -1;
+            }
+            if (d1.substring(3, 5).compareTo(d2.substring(3,5)) < 0){
+                return 1;
+            }
+            if (d1.substring(0, 2).compareTo(d2.substring(0,2)) < 0){
+                return -1;
+            }
+            return 0;
+        };
+
         printArray(dates, "Before Sorting");
-
         Arrays.sort(dates, comparator);
-
         printArray(dates, "After Sorting:");
+        printArray(expected, "Expected:");
+        assertArrayEquals(expected, dates);
 
-//        assertArrayEquals(expected, dates);
-
+//        System.out.println(dateParser("15-11-2010",0, 2));
 
     }
+
+    private String dateParser(String date, int indexA, int indexB){
+        return date.substring(indexA, indexB);
+
+    }
+
 
 
     private void printArray(String[] arr, String title) {
