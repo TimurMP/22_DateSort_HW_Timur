9887e55e7013091aa611fd0ed67188f937acbff4
TimurMP <titimurmamyshev@gmail.commur>
2022-06-24 10:55:02 +0300

    added printArray

--- src/telran/datesort/tests/DateSortTest.java
+++ src/telran/datesort/tests/DateSortTest.java
@@ -25,13 +25,24 @@ class DateSortTest {
                 "15-01-2010",
                 "28-01-2010",
         };
-        Comparator<String> comparator = (d1,d2) -> 0;
+        Comparator<String> comparator = (d1, d2) -> 0;
+        printArray(dates, "Before Sorting");
 
         Arrays.sort(dates, comparator);
+
+        printArray(dates, "After Sorting:");
 
 //        assertArrayEquals(expected, dates);
 
 
+    }
 
+
+    private void printArray(String[] arr, String title) {
+        System.out.println(title);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("\n");
     }
 }
